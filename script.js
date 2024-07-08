f06bc8da3427915bf6d6f4e97e04f12cc9116777
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-07-08 16:40:52 +0000

    Second Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const danceButton = document.getElementById('danceButton');
+const feedButton = document.getElementById('feedButton');
+const petButton = document.getElementById('petButton');
+const rockImage = document.getElementById('rockImage');
+const rockStatus = document.getElementById('rockStatus');
+
+let rockState = 'Content'; // Initial rock state
+
+function updateRockStatus(newState) {
+  rockState = newState;
+  rockStatus.textContent = newState;
+  if (newState === 'Tired') {
+    rockImage.src = 'tired_rock.jpg';
+  } else if (newState === 'Sad') {
+    rockImage.src = 'sad_rock.jpg';
+  } else {
+    rockImage.src = 'mr_rock.jpg'; // Default image
+  }
+}
+
+danceButton.addEventListener('click', () => {
+  rockImage.src = 'dance_rock.jpg'; // Change the image source
